835eb4d7b4687290a6ea79ab8be5d955c1c512ad
eduardolizarazu <jelo2021@gmail.com>
2022-08-20 23:52:28 -0400

    finish to setup the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+    mode: 'production',
     entry: "./src/index.js",
     output : {
         path : path.resolve(__dirname, "dist"),
@@ -8,5 +11,34 @@ module.exports = {
     },
     resolve : {
         extensions: [".js"]
-    }
+    },
+    module : {
+        rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader"
+              },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader", 
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({ 
+            inject: true, 
+            template: './public/index.html', 
+            filename: './index.html'
+        })
+    ]
 }
