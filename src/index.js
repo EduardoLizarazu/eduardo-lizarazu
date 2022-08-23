f0cbc0edfe7e70b643a13e55c2fce832615c5d51
eduardolizarazu <jelo2021@gmail.com>
2022-08-23 19:15:11 -0400

    Implement Typing to the Frontend Developer and switched with backend developer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
 import "./main.scss";
-console.log("Hello world!");
+
+
+const typeTextSpan = document.querySelector(".typed-text");
+
+const textArray = ["Frontend Developer", "Backend Developer"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length) {
+        typeTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typeTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) {
+            textArrayIndex = 0;
+        }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, newTextDelay + 250);
+});
